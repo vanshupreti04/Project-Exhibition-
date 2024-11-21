d25523cda3a57fbd8fe3bc256641ed85a1da64d4
vanshupreti04 <vanshupreti0412@gmail.com>
2024-11-21 22:19:29 +0530

    Update script.js and buttons are working. Rest backend done by it yourself

--- Project Exhibition/script.js
+++ Project Exhibition/script.js
@@ -10,9 +10,33 @@ function showImage(index) {
         }
     });
 }
+
+/*Sliding Screen*/
+
 function nextImage() {
     currentImageIndex = (currentImageIndex < images.length - 1) ? currentImageIndex + 1 : 0;
     showImage(currentImageIndex);
 }
 showImage(currentImageIndex);
 setInterval(nextImage, slideInterval);
+
+/*Raising the ticket and Finding a ticket*/
+
+document.getElementById('raiseTicketButton').onclick = function() {
+    window.location.href = 'raise_ticket_page.html'; // Redirect to Raise a Ticket page
+};
+document.getElementById('findTicketButton').onclick = function() {
+    window.location.href = 'find_ticket_page.html'; // Redirect to Find a Ticket page
+};
+
+/*Features Button Functionable*/
+
+document.getElementById('viewOrganisation').onclick = function() {
+    window.location.href = 'view_organisation.html'; // Redirect to Find a Ticket page
+};
+document.getElementById('itemAvailable').onclick = function() {
+    window.location.href = 'item_available.html'; // Redirect to Find a Ticket page
+};
+document.getElementById('startTracking').onclick = function() {
+    window.location.href = 'start_tracking.html'; // Redirect to Find a Ticket page
+};
